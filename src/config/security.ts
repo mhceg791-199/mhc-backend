63b3b8554cf5c0908d7d435b8d3036ce35437e43
mhceg791-199 <mhceg791@gmail.com>
2026-03-24 15:31:23 +0200

    add mosaic eng

--- src/config/security.ts
+++ src/config/security.ts
@@ -21,7 +21,7 @@ export const helmetConfig = helmet({
 export const corsConfig = cors({
   origin: ENV.isProduction
     ? [ENV.clientUrl]
-    : [ENV.clientUrl, "https://auth-client-three-umber.vercel.app/", "http://localhost:5173", "http://localhost:3000"],
+    : [ENV.clientUrl, "https://auth-client-three-umber.vercel.app/", "http://localhost:5173", "http://localhost:3000" , "https://mosaic-eng.com"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
